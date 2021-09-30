f01597e871ebe96527eb0f6f2f23a0a15fe68bb8
Tran Nguyen Thuong Truong <ititiu19228@student.hcmiu.edu.vn>
2021-09-30 08:22:17 +0700

    feat: add accountTest class
    This is main class to control and run source code that calculate banking money and salary

---
+++ Bank Life Cycle/AccounTest.java
@@ -0,0 +1,26 @@
+package thuongtruong1009;
+
+public class AccounTest {
+
+	public static void main(String[] args) {
+		SavingAccount momsSavings = new SavingAccount(0.5);	
+		CheckingAccount harrysChecking = new CheckingAccount(100);	
+		
+       	momsSavings.deposit(10000);
+       	momsSavings.transfer(2000, harrysChecking);     
+       	
+		harrysChecking.withdraw(1500);
+		harrysChecking.withdraw(80);      
+		
+		momsSavings.transfer(1000, harrysChecking);
+		
+		harrysChecking.withdraw(400);      
+		
+		momsSavings.addInterest();
+		
+		harrysChecking.deductFees();
+		
+		System.out.println("Savings balance = "+ momsSavings.getBalance());
+		System.out.println("Checking balance = " + harrysChecking.getBalance());
+	}
+}
