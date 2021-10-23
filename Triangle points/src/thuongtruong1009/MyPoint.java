4d3a0fac3ead0ef0ae579025df73e8f32887d28c
Tran Nguyen Thuong Truong <ititiu19228@student.hcmiu.edu.vn>
2021-10-23 09:55:42 +0700

    Create MyPoint.java

---
+++ Triangle points/src/thuongtruong1009/MyPoint.java
@@ -0,0 +1,22 @@
+package thuongtruong1009;
+
+public class MyPoint {
+	private int x;
+	private int y;
+	public MyPoint(int x, int y) {
+		this.x= x;
+		this.y= y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x= x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y= y;
+	}
+}
